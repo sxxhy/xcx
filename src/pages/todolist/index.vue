0fbb986433fd48d77e60d2d9a9966d1840412377
sxxhy <1016522769@qq.com>
2019-01-10 21:39:34 +0800

    mpvue小程序

---
+++ src/pages/todolist/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      hello
+      <ToDoList></ToDoList>
+    </div>
+</template>
+
+<script>
+  import ToDoList from '@/components/ToDoList'
+  export default {
+    name: 'index',
+    components: {
+      ToDoList
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
